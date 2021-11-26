e3cdce578f317843991072b7f0f7bd5b98212a86
Omar <omaralhoori@gmail.com>
2021-11-26 14:35:27 +0300

    add background images

--- lib/views/contact_view.dart
+++ lib/views/contact_view.dart
@@ -26,142 +26,167 @@ class _ContactViewState extends State<ContactView> {
     GlobalKey<FormBuilderState> _fbKey = GlobalKey<FormBuilderState>();
     return BaseView<HomeViewModel>(
       builder: (context, home, _) {
-        return Scaffold(
-          appBar: AppBar(
-              backgroundColor: Palette.appbarBackgroundColor,
-              leading: BackButton(
-                color: Palette.appbarForegroundColor,
-              ),
-              title: Text(
-                tr('Contact Us'),
-                style: TextStyle(color: Palette.appbarForegroundColor),
-              )),
-          body: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 20,
+        return Stack(
+          children: [
+            Image(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                fit: BoxFit.cover,
+                image: AssetImage('assets/contact_bg.png')),
+            Scaffold(
+              appBar: AppBar(
+                  backgroundColor: Palette.appbarBackgroundColor,
+                  leading: BackButton(
+                    color: Palette.appbarForegroundColor,
                   ),
-                  FormBuilder(
-                    key: _fbKey,
-                    child: Column(
-                      children: [
-                        buildDecoratedControl(
-                          control: FormBuilderTextField(
-                            name: 'sender_name',
-                            validator: FormBuilderValidators.compose([
-                              FormBuilderValidators.required(context),
-                            ]),
-                            decoration: Palette.formFieldDecoration(
-                              label: tr("Sender Name"),
+                  title: Text(
+                    tr('Contact Us'),
+                    style: TextStyle(color: Palette.appbarForegroundColor),
+                  )),
+              body: SizedBox.expand(
+                child: Container(
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          fit: BoxFit.contain,
+                          image: AssetImage('assets/contact_bg.png'),
+                          alignment: Alignment.bottomCenter)),
+                  child: SingleChildScrollView(
+                    child: Container(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: 20,
+                          ),
+                          FormBuilder(
+                            key: _fbKey,
+                            child: Column(
+                              children: [
+                                buildDecoratedControl(
+                                  control: FormBuilderTextField(
+                                    name: 'sender_name',
+                                    validator: FormBuilderValidators.compose([
+                                      FormBuilderValidators.required(context),
+                                    ]),
+                                    decoration: Palette.formFieldDecoration(
+                                      label: tr("Sender Name"),
+                                    ),
+                                  ),
+                                  field: DoctypeField(
+                                      fieldname: "sender_name",
+                                      label: tr("Sender Name")),
+                                ),
+                                buildDecoratedControl(
+                                  control: FormBuilderTextField(
+                                    name: 'email',
+                                    validator: FormBuilderValidators.compose([
+                                      FormBuilderValidators.required(context),
+                                      FormBuilderValidators.email(context)
+                                    ]),
+                                    decoration: Palette.formFieldDecoration(
+                                      label: tr("Email Address"),
+                                    ),
+                                  ),
+                                  field: DoctypeField(
+                                      fieldname: "email",
+                                      label: tr("Email Address")),
+                                ),
+                                buildDecoratedControl(
+                                  control: FormBuilderTextField(
+                                    name: 'subject',
+                                    validator: FormBuilderValidators.compose([
+                                      FormBuilderValidators.required(context),
+                                    ]),
+                                    decoration: Palette.formFieldDecoration(
+                                      label: tr("Subject"),
+                                    ),
+                                  ),
+                                  field: DoctypeField(
+                                      fieldname: "subject", label: tr("Subject")),
+                                ),
+                                buildDecoratedControl(
+                                    control: FormBuilderTextField(
+                                      name: 'message',
+                                      keyboardType: TextInputType.multiline,
+                                      textInputAction: TextInputAction.newline,
+                                      minLines: 5,
+                                      maxLines: 5,
+                                      validator: FormBuilderValidators.compose([
+                                        FormBuilderValidators.required(context),
+                                      ]),
+                                      decoration: Palette.formFieldDecoration(
+                                          hint: tr("Enter your message"),
+                                          label: tr("Message"),
+                                          fillColor: Color(0xEaFFFFFF)),
+                                    ),
+                                    field: DoctypeField(
+                                        fieldname: "message",
+                                        label: tr("Message"))),
+                                Container(
+                                  margin: EdgeInsets.symmetric(vertical: 16),
+                                  child: FrappeFlatButton(
+                                      onPressed: () async {
+                                        if (_fbKey.currentState != null) {
+                                          if (_fbKey.currentState!
+                                              .saveAndValidate()) {
+                                            var formValue =
+                                                _fbKey.currentState?.value;
+                                            if (formValue == null) return;
+                                            String deviceId =
+                                                await Palette.deviceID();
+                                            ContactMessageRequest request =
+                                                ContactMessageRequest(
+                                                    senderName:
+                                                        formValue["sender_name"],
+                                                    email: formValue["email"],
+                                                    subject: formValue["subject"],
+                                                    message: formValue["message"],
+                                                    user: deviceId);
+                                            var response = await home
+                                                .sendContactMessage(request);
+                                            if (response["message"] != null) {
+                                              FrappeAlert.successAlert(
+                                                  title: tr(response["message"]),
+                                                  context: context);
+                                              setState(() {
+                                                _fbKey.currentState!.patchValue({
+                                                  "sender_name": "",
+                                                  "email": "",
+                                                  "subject": "",
+                                                  "message": "",
+                                                });
+                                              });
+                                            } else if (response["errorMessage"] !=
+                                                null) {
+                                              FrappeAlert.errorAlert(
+                                                  title: tr(
+                                                      response["errorMessage"]),
+                                                  context: context);
+                                            } else {
+                                              FrappeAlert.errorAlert(
+                                                  title:
+                                                      tr("Something went wrong"),
+                                                  context: context);
+                                            }
+                                          }
+                                        }
+                                      },
+                                      fullWidth: true,
+                                      height: 40,
+                                      buttonType: ButtonType.primary,
+                                      title: tr("Send Message")),
+                                )
+                              ],
                             ),
                           ),
-                          field: DoctypeField(
-                              fieldname: "sender_name",
-                              label: tr("Sender Name")),
-                        ),
-                        buildDecoratedControl(
-                          control: FormBuilderTextField(
-                            name: 'email',
-                            validator: FormBuilderValidators.compose([
-                              FormBuilderValidators.required(context),
-                              FormBuilderValidators.email(context)
-                            ]),
-                            decoration: Palette.formFieldDecoration(
-                              label: tr("Email Address"),
-                            ),
-                          ),
-                          field: DoctypeField(
-                              fieldname: "email", label: tr("Email Address")),
-                        ),
-                        buildDecoratedControl(
-                          control: FormBuilderTextField(
-                            name: 'subject',
-                            validator: FormBuilderValidators.compose([
-                              FormBuilderValidators.required(context),
-                            ]),
-                            decoration: Palette.formFieldDecoration(
-                              label: tr("Subject"),
-                            ),
-                          ),
-                          field: DoctypeField(
-                              fieldname: "subject", label: tr("Subject")),
-                        ),
-                        buildDecoratedControl(
-                            control: FormBuilderTextField(
-                              name: 'message',
-                              keyboardType: TextInputType.multiline,
-                              textInputAction: TextInputAction.newline,
-                              minLines: 5,
-                              maxLines: 5,
-                              validator: FormBuilderValidators.compose([
-                                FormBuilderValidators.required(context),
-                              ]),
-                              decoration: Palette.formFieldDecoration(
-                                hint: tr("Enter your message"),
-                                label: tr("Message"),
-                              ),
-                            ),
-                            field: DoctypeField(
-                                fieldname: "message", label: tr("Message"))),
-                        Container(
-                          margin: EdgeInsets.symmetric(vertical: 16),
-                          child: FrappeFlatButton(
-                              onPressed: () async {
-                                if (_fbKey.currentState != null) {
-                                  if (_fbKey.currentState!.saveAndValidate()) {
-                                    var formValue = _fbKey.currentState?.value;
-                                    if (formValue == null) return;
-                                    String deviceId = await Palette.deviceID();
-                                    ContactMessageRequest request =
-                                        ContactMessageRequest(
-                                            senderName:
-                                                formValue["sender_name"],
-                                            email: formValue["email"],
-                                            subject: formValue["subject"],
-                                            message: formValue["message"],
-                                            user: deviceId);
-                                    var response =
-                                        await home.sendContactMessage(request);
-                                    if (response["message"] != null) {
-                                      FrappeAlert.successAlert(
-                                          title: tr(response["message"]),
-                                          context: context);
-                                      setState(() {
-                                        _fbKey.currentState!.patchValue({
-                                          "sender_name": "",
-                                          "email": "",
-                                          "subject": "",
-                                          "message": "",
-                                        });
-                                      });
-                                    } else if (response["errorMessage"] !=
-                                        null) {
-                                      FrappeAlert.errorAlert(
-                                          title: tr(response["errorMessage"]),
-                                          context: context);
-                                    } else {
-                                      FrappeAlert.errorAlert(
-                                          title: tr("Something went wrong"),
-                                          context: context);
-                                    }
-                                  }
-                                }
-                              },
-                              fullWidth: true,
-                              height: 40,
-                              buttonType: ButtonType.primary,
-                              title: tr("Send Message")),
-                        )
-                      ],
+                        ],
+                      ),
                     ),
                   ),
-                ],
+                ),
               ),
             ),
-          ),
+          ],
         );
       },
     );
--- lib/views/settings/settings_view.dart
+++ lib/views/settings/settings_view.dart
@@ -8,63 +8,75 @@ class SettingsViews extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final Size size = MediaQuery.of(context).size;
     return BaseView<SettingsViewModel>(
       builder: (context, settings, _) {
         // settings.chosenValue = context.locale.languageCode;
         return Scaffold(
           appBar: AppBar(),
-          body: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  SizedBox(width: size.width * .1,),
-                  Flexible(
-                    flex: 1,
-                    child: Text(
-                      tr("Language") + ": ",
-                      style: Theme.of(context).textTheme.bodyText1,
+          body: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/settings_bg.png'))),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: size.width * .1,
                     ),
-                  ),
-                  SizedBox(width: size.width * .05,),
-                  Flexible(
-                    flex: 2,
-                    child: DropdownButtonFormField<String>(
-                      focusColor:Colors.white,
-                      value: settings.chosenValue ?? context.locale.languageCode,
-                      //elevation: 5,
-                      style: TextStyle(color: Colors.white),
-                      decoration: InputDecoration(
-                        // prefixIcon: FaIcon(FontAwesomeIcons.language),
-                        prefixIcon: Icon(Icons.language),
-                        border: InputBorder.none,
-
+                    Flexible(
+                      flex: 1,
+                      child: Text(
+                        tr("Language") + ": ",
+                        style: Theme.of(context).textTheme.bodyText1,
                       ),
-                      iconEnabledColor:Colors.black,
-                      items: settings.items.map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value,style:TextStyle(color:Colors.black),),
-                        );
-                      }).toList(),
-                      hint:Text(
-                        tr("Language"),
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500),
-                      ),
-                      onChanged: (value) async{
-                        await context.setLocale(Locale('$value'));
-                        settings.chosenValue = value!;
-                      },
                     ),
-                  ),
-                ],
-              ),
-            ],
+                    SizedBox(
+                      width: size.width * .05,
+                    ),
+                    Flexible(
+                      flex: 2,
+                      child: DropdownButtonFormField<String>(
+                        focusColor: Colors.white,
+                        value:
+                            settings.chosenValue ?? context.locale.languageCode,
+                        //elevation: 5,
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          // prefixIcon: FaIcon(FontAwesomeIcons.language),
+                          prefixIcon: Icon(Icons.language),
+                          border: InputBorder.none,
+                        ),
+                        iconEnabledColor: Colors.black,
+                        items: settings.items
+                            .map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(
+                              value,
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          );
+                        }).toList(),
+                        hint: Text(
+                          tr("Language"),
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500),
+                        ),
+                        onChanged: (value) async {
+                          await context.setLocale(Locale('$value'));
+                          settings.chosenValue = value!;
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
           bottomNavigationBar: Padding(
             padding: const EdgeInsets.only(bottom: 7.0),
